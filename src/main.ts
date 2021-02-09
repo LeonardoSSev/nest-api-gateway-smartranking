9cfd3d18d5a21923be04dd2a5f96f9be0c0a19aa
LeonardoSSev <leonardossev@gmail.com>
2021-02-09 12:44:31 -0300

    config: Sets dotenv, filter and the application's timezone

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+require('dotenv').config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as momentTimezone from 'moment-timezone';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  defineTimezone();
+
+  await app.listen(process.env.APP_PORT);
 }
+
+function defineTimezone() {
+  return Date.prototype.toJSON = function(): any {
+    return momentTimezone(this)
+      .tz(process.env.APP_DATE_TIMEZONE)
+      .format(process.env.APP_DATE_FORMAT);
+  }
+}
+
+
 bootstrap();
