81e69a812c7ed62465a4a4446ab350edc3ea357a
LeonardoSSev <leonardossev@gmail.com>
2021-02-09 15:02:36 -0300

    feat: Adds the find by id/list all categories

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Logger, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post, UsePipes } from '@nestjs/common';
 import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 import { CreateCategoryDTO } from './dtos/categories/create-category.dto';
 import { RequestParameterValidation } from './pipes/RequestParameterValidation.pipe';
 
@@ -24,6 +25,16 @@ export class AppController {
   @UsePipes(RequestParameterValidation)
   async createCategory(@Body() createCategoryDTO: CreateCategoryDTO) {
     return await this.clientAdminBackend.emit('create-category', createCategoryDTO);
+  }
+
+  @Get('categories')
+  listCategories(): Observable<any> {
+    return this.clientAdminBackend.send('list-categories', '');
+  }
+
+  @Get('categories/:id')
+  findCategory(@Param('id') _id: string): Observable<any> {
+    return this.clientAdminBackend.send('find-category', _id)
   }
 
 }
